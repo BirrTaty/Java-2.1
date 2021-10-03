230f7db471440f604d2d579036255d3c3e59d16b
Tatiana <birrtaty@gmail.com>
2021-10-03 21:58:49 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 400;
+        int millePrice = 20;
+        int numberOfMilles = ticketPrice / millePrice;
+
+        System.out.println("Количество бонусных милль: " + numberOfMilles);
+    }
 }
